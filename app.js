cf49274c0e2dc9694d67b2546410bb2649368ea5
TayyabKhan1221 <Tayyabkhano603@gmail.com>
2024-05-19 11:25:13 +0500

    objects

---
+++ app.js
@@ -0,0 +1,20 @@
+// let sym = symbol()
+
+let user = {
+ram: 8,
+model: "test",
+company: "iphone",
+company: "samsung",
+isGaming: false,
+nested: {name: "tayyab", age: "20",},
+sym: "ma 1 wed develpmnet ho",
+city: "karachi",
+friend: "100 ",  
+}
+  
+let a = "company"
+
+console.log(user.isGaming)
+console.log(user.friend)
+// console.log(user.[a])
+console.log(user?.[a])
